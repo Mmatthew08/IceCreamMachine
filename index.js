bfb914a2dae302c7fca29f4e895532ba689db656
Matthew Eski <metehaneski08@gmail.com>
2023-10-18 13:33:30 -0500

    Need to finish the last part

---
+++ index.js
@@ -0,0 +1,13 @@
+const avaliableFlavors=['chocolate','vanilla','banana','strawberry' ,'caramel']
+function findFlavor(){
+    let a= prompt('Enter the flavor that you want')
+    if (a= 'chocolate'|| 'vanilla'||'banana'||'strawberry'||'caramel')
+    console.log ('Item is available')
+else if (a!=chocolate'|| 'vanilla'||'banana'||'strawberry'||'caramel') console.log('Item not available')
+}
+findFlavor();
+
+const orders=['chocolate','vanilla','banana','strawberry' ,'caramel']
+//return 'function findFlavor'
+
+
